438215cd50db742f155a5b084052b59397834f5f
Ankit Baghel <144383065+AnkitBaghel93@users.noreply.github.com>
2025-07-03 14:49:31 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,19 +8,32 @@ const ticketRoutes = require('./routes/ticketRoutes');
 const userRoutes = require('./routes/userRoutes'); 
 
 
-
-
-
-
-
 // App config
 const app = express();
 
 
 //  Connect to DB at startup
 conn();
-app.use(cors());
+const allowedOrigins = [
+  'https://ticketbooking-frontend.onrender.com',
+  'http://localhost:5000' // Optional: for development testing
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin like mobile apps or curl
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
 app.use(express.json());
+
+
 
 //  route 
 app.use('/api/auth', authRoutes);
@@ -28,12 +41,15 @@ app.use('/api/tickets', ticketRoutes);
 app.use('/api/users', userRoutes);
 
 
-
 const PORT = 5000;
 // Start server
 app.listen(PORT, () => {
   console.log(` Server is running on http://localhost:${PORT}`);
 });
+
+
+
+
 
 
 
