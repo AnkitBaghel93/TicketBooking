82b8436410c3519d02edd8343f79ff080bb4193b
Ankit Baghel <144383065+AnkitBaghel93@users.noreply.github.com>
2025-07-05 02:51:55 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { register, login , getAllUsers  } = require('../controllers/authController');
+const { register, login , getAllUsers , logoutUser , forgotPassword,  resetPassword} = require('../controllers/authController');
 
 router.post('/signup', register);
 router.post('/signin', login);
+router.post('/logout', logoutUser);
 
-
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password/:token', resetPassword);
 
 module.exports = router;
+
+
+
