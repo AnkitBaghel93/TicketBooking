8003992dc6efefa7159f5d3172d4b42dd7618814
Ankit Baghel <144383065+AnkitBaghel93@users.noreply.github.com>
2025-07-05 02:44:10 +0530

    Update Performance.jsx

--- frontend/src/pages/TechnicalSupport/Performance.jsx
+++ frontend/src/pages/TechnicalSupport/Performance.jsx
@@ -9,7 +9,7 @@ useEffect(() => {
   const fetchTickets = async () => {
     const token = localStorage.getItem('token');
     try {
-      const res = await fetch('http://localhost:5000/api/tickets/technical', {
+      const res = await fetch('https://ticketbooking-backend-uq35.onrender.com/api/tickets/technical', {
         headers: {
           Authorization: `Bearer ${token}`
         }
